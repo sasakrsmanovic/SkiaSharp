7a67a76307485c244ac3a1380f12242edace7401
GitHub Action <action@github.com>
2026-04-27 23:43:08 +0000

    Deploy staging docs for PR #3763
    https://mono.github.io/SkiaSharp/staging/3763/

--- staging/3763/gallery-uno/service-worker.js
+++ staging/3763/gallery-uno/service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('12458a6d-0985-41bd-99c9-6d0c6682f1c2').then(async function (cache) {
+            caches.open('10df205d-3118-48af-80f8-aa175857a5e5').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== '12458a6d-0985-41bd-99c9-6d0c6682f1c2';
+                        return cacheName !== '10df205d-3118-48af-80f8-aa175857a5e5';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
