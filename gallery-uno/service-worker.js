a71349539450e9cb1a9b5ba6c5f25549e6c89baf
GitHub Action <action@github.com>
2026-04-28 15:58:21 +0000

    Deploy main documentation from commit 6a020ad32749125c38135d92aad177c35fb5582f

--- gallery-uno/service-worker.js
+++ gallery-uno/service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('bdf2e02e-0b83-4931-a249-7cb58d901308').then(async function (cache) {
+            caches.open('55842a82-6e0d-448b-bd6f-834c7c40975d').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== 'bdf2e02e-0b83-4931-a249-7cb58d901308';
+                        return cacheName !== '55842a82-6e0d-448b-bd6f-834c7c40975d';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
