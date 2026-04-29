65c9dddeb5f2426d811433e5c42b1c844407a295
GitHub Action <action@github.com>
2026-04-29 22:07:13 +0000

    Deploy main documentation from commit df9d14de08183b04958471fa86b6fc7572423b78

--- fiddle/service-worker.js
+++ fiddle/service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('9ae92a11-df3f-420b-b054-9778b5c1c7dd').then(async function (cache) {
+            caches.open('82736544-15db-462d-a1a0-fc48fd822d44').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== '9ae92a11-df3f-420b-b054-9778b5c1c7dd';
+                        return cacheName !== '82736544-15db-462d-a1a0-fc48fd822d44';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
--- gallery-uno/service-worker.js
+++ gallery-uno/service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('fe70efd0-5320-44b0-a15e-fd468884a783').then(async function (cache) {
+            caches.open('e663aa95-c9d8-425e-99da-dd90a4e9ece0').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== 'fe70efd0-5320-44b0-a15e-fd468884a783';
+                        return cacheName !== 'e663aa95-c9d8-425e-99da-dd90a4e9ece0';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
