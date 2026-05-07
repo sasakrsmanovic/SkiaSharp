2fc333ea673e7b7f6c5abf86fcf040c639b0187a
GitHub Action <action@github.com>
2026-05-07 15:24:26 +0000

    Deploy main documentation from commit c53542b809cdbb824da48e2f838e476d7c895a1d

--- fiddle/service-worker.js
+++ fiddle/service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('cdd431f9-fb6b-49ad-914e-65548fab4d5d').then(async function (cache) {
+            caches.open('29d6c354-103b-496e-9dfd-a87c34d26227').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== 'cdd431f9-fb6b-49ad-914e-65548fab4d5d';
+                        return cacheName !== '29d6c354-103b-496e-9dfd-a87c34d26227';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
